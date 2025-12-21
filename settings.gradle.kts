e6a77e318ea408b01195c5f1a401f9a0420decc6
tofik.07 <slodkitofik@gmail.com>
2025-12-21 02:38:56 +0100

    chore: update template to 1.21.11 (#8)

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -3,6 +3,7 @@ import java.util.Locale
 pluginManagement {
     repositories {
         gradlePluginPortal()
+        maven("https://maven.canvasmc.io/snapshots") // TODO: remove me when switching to stable weaver
         maven("https://maven.canvasmc.io/releases")
     }
 }
